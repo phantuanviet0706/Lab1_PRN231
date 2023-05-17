ec33b6037c53f8bc0127812585e6c5f31a51fdce
Viet <phantuanviet0706@gmail.com>
2023-05-17 21:35:24 +0700

    Add project files.

---
+++ BusinessObjects/MyDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext() { }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+          : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", true, true)
+               .Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("MystoreDB"));
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get;set; }
+        protected override void OnModelCreating(ModelBuilder optionBuilder)
+        {
+            optionBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Beverages"},
+                new Category { CategoryId = 2, CategoryName = "Condiments" },
+                new Category { CategoryId = 3, CategoryName = "Confections" },
+                new Category { CategoryId = 4, CategoryName = "Dairy Products" },
+                new Category { CategoryId = 5, CategoryName = "Grains/Cerearls" },
+                new Category { CategoryId = 6, CategoryName = "Meat/Poultry" },
+                new Category { CategoryId = 7, CategoryName = "Produce" },
+                new Category { CategoryId = 8, CategoryName = "Seefood" }
+            );
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# HE153199 Trần Ngọc Hiếu
+# -Lab01_ASP.NETCoreWebAPI
+repo: https://github.com/HieuTNgoc/Lab01_ASP.NETCoreWebAPI
